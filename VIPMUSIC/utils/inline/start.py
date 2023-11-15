54def0a1daa53a2b4abbd163999563afcbaa2af5
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-15 11:14:04 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/vip_boy_dm")
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/")
         ],
     ]
     return buttons
