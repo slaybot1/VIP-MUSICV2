71137a69a7b0fb3313f8b074ddeb63d4a0566188
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-11-16 16:18:16 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -97,7 +97,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            random.choice(YUMI_PICS),
+            photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
